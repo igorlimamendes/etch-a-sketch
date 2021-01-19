c77d7ef5524828ffa02a826d0ea22ebed4a77a5e
igorlimamendes <igorlimamendes@protonmail.com>
2021-01-19 10:36:05 -0300

    Edit commentary

--- js/script.js
+++ js/script.js
@@ -18,19 +18,19 @@ function makeRows(rows, cols) {
     }
 }
 
-makeRows(10, 10); // Default value when user reload webpage or access for the first time
+makeRows(10, 10); // Default value when user reload or access page for the first time
 
 // Button function
 button.addEventListener("click", () => {
     let size = prompt("Select a value between 1 and 100");
 
-    if (size >= 1 && size <= 100) { // User have enter a valid value
+    if (size >= 1 && size <= 100) {
         let totLength = container.children.length; // Get total length of container children that have been created in makeRows function
         
         for (let i = 0; i < totLength; i++) {
-            let gridItem = document.querySelector(".grid-item"); // Get class of container child
+            let gridItem = document.querySelector(".grid-item");
             
-            container.removeChild(gridItem); // Remove container child
+            container.removeChild(gridItem);
         }
 
         makeRows(size, size); // Call function to create a game with new values
